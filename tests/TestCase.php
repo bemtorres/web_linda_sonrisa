71d9795e72658d9bcc8c7020bcaecbef6835f9c9
Bemtorres <benja.mora.torres@gmail.com>
2019-10-10 13:38:53 -0300

    update

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,3 +8,6 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 }
+
+// select 'drop table ' || table_name || ';' from user_tables;
+// select 'drop sequence ' || sequence_name || ';' from user_sequences;
