677aa79055eadf5b675ae448f6c48f3d387e6ccb
Bemtorres <benja.mora.torres@gmail.com>
2019-11-04 16:12:24 -0300

    update

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -144,11 +144,11 @@
                         <span class="sub-item">Boleta de servicio</span>
                     </a>
                 </li>
-                <li>
+                {{-- <li>
                     <a href="charts/sparkline.html">
                         <span class="sub-item">Estados</span>
                     </a>
-                </li>
+                </li> --}}
             </ul>
         </div>
     </li>
